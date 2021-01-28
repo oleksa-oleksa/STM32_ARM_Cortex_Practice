d2739b113fc3c17f8e2d98aec2548f959f868681
Oleksandra Baga <oleksandra.baga@gmail.com>
2021-01-28 20:05:38 +0100

    deleted code from old assignments

--- 3d-srld_mpp/main.c
+++ 3d-srld_mpp/main.c
@@ -9,16 +9,6 @@ char counter_char;
 
 int main(void)
 {
-
-    // Assignment 5
-    // Window Watchdog
-    /*
-    unsigned char value_watchdog_counter = 0x7f;
-    unsigned char window_value = 0x50;
-    unsigned char window_value_refresh = 0x50;
-    unsigned char cnt_i = 0;
-    unsigned char cnt_j = 0;
-    */
     // Initialization of the system and the clock system
     SystemInit();
 
@@ -31,24 +21,24 @@ int main(void)
     // Release of interrupts
     our_init_board();
 
-    //beep(4000,200,0); // test beep
-
-    // assignment 2
-    //init_leds(RCC_AHB1Periph_GPIOB, GPIOB, GPIO_Pin_2);
-    //init_button_1();
-    //init_button_2();
-
     // assignment 5: Independent Watchdog
     //init_iwdg();
 
     // Assignment 5
     // Window Watchdog
-    /*
+    unsigned char value_watchdog_counter = 0x7f;
+    unsigned char window_value = 0x50;
+    unsigned char window_value_refresh = 0x50;
+    unsigned char cnt_i = 0;
+    unsigned char cnt_j = 0;
+
+    // Assignment 5
+    // Window Watchdog
+
     RCC_APB1PeriphClockCmd(RCC_APB1Periph_WWDG, ENABLE);
     WWDG_SetPrescaler(WWDG_Prescaler_8);
     WWDG_SetWindowValue(window_value);
     WWDG_Enable(value_watchdog_counter);
-    */
 
     init_button_2();
 
@@ -60,8 +50,7 @@ int main(void)
     counter=0;
     char usart2_tx_buffer[50];
 
-    //cnt_i = (unsigned char) (value_watchdog_counter + 1);
-
+    cnt_i = (unsigned char) (value_watchdog_counter + 1);
 
     while(1){
 
@@ -96,24 +85,8 @@ int main(void)
             usart2_print("Taste2 gedrückt\r\n");
             wait_uSek(5000000);
         }
-        */
-        //counter_char = counter+'0'; // convert int to char by building ascci value of char. 1+'0'=='1'
-        // Assignment 4 task 2.3.
-        //usart2_send_text(&counter_char);
-        // Assignment 4, task 2.5
-        //usart2_print(": Cora und Oleksandra, Frohe Weihnachten!!");
-        //usart2_send_text("\r\n");
-        //counter = (counter +1) % 10;
-
-        // for assignment 4, task 2.6 the previous lines
-        // should be commented to free uart
-        // from looped output
-
-        // We set NVIC_InitStructure.NVIC_IRQChannel = USART2_IRQn;
-        // and implement everything in IRQ_Handler
-
-        //timer = 1000; // 1 second
-        //while (timer) {;}
-    }
+           //while (timer) {;}
+    */
     return 0; // to make the warning stop
 	}
+}
